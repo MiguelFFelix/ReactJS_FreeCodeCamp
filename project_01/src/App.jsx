13bb459cbbc80ce371c6eff8a9af3183aebc2793
Miguel Felix <miguelffelix5@gmail.com>
2025-08-04 10:39:40 -0300

    Add files of React projects

---
+++ project_01/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Navbar';
+import Main from './components/Main';
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
---
+++ project_01/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt=""/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
