fa5f314ae138753f989bedb2189b8d39cfafe466
Miguel Felix <miguelffelix5@gmail.com>
2025-08-04 10:51:26 -0300

    Add new Navbar component and update App structure

---
+++ project_02/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <header>
+            <nav>
+                <span>My Website</span>
+            </nav>
+        </header>
+    )
+}
--- project_02/src/index.jsx
+++ project_02/src/index.jsx
@@ -1,5 +1,8 @@
 import {createRoot} from 'react-dom/client';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render()
+root.render(
+    <App />
+)
