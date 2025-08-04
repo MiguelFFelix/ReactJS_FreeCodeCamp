887e5701bc2f10c8c4a9e74b9feb409bcbd70c7c
Miguel Felix <miguelffelix5@gmail.com>
2025-08-04 11:02:48 -0300

    Modification of files in the ReactJS project to implement a basic Navbar component and ensure the application renders correctly.

--- project_02/src/index.jsx
+++ project_02/src/index.jsx
@@ -1,5 +1,6 @@
 import {createRoot} from 'react-dom/client';
 import App from './App';
+import './index.css'; // Importing the CSS file
 
 const root = createRoot(document.getElementById('root'));
 
