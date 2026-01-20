fdc22efa1d7b26318ecb34e98de246b0cfa8c5bf
WebDev_Young <78152130+MiguelFFelix@users.noreply.github.com>
2026-01-20 08:37:36 -0300

    Finishing Travel Journal Project and rename projects

--- Travel_Journal/src/components/Entry.jsx
+++ Travel_Journal/src/components/Entry.jsx
@@ -4,17 +4,17 @@ export default function Entry(props){
     return (
         <article className="journal-entry">
             <div className="main-image-container">
-                <img className="main-image" src={props.img} alt={props.alt} />
+                <img className="main-image" src={props.entry.img.src} alt={props.entry.img.alt} />
             </div>
             <div className="info-container">
                 <div className="location-info">
                     <img className="marker" src={markerImg} alt="location icon" />
-                    <span className="country">{props.country}</span>
-                    <a href={props.googleMapsLink}>View on Google Maps</a>
+                    <span className="country">{props.entry.country}</span>
+                    <a href={props.entry.googleMapsLink}>View on Google Maps</a>
                 </div>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="entry-text">{props.dates}</p>
-                <p>{props.text}</p>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="entry-text">{props.entry.dates}</p>
+                <p>{props.entry.text}</p>
             </div>
         </article>
 
